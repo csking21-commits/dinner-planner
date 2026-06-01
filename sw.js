c4df11be7dbbfc2935f00e9c7d0eae632e437549
csking21-commits <csking21@gmail.com>
2026-06-01 18:54:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'dinner-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = [/dinner-planner'/', /dinner-planner'/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
